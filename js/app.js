de3e2e2d99a57cd3ab68cf89723c5ea4e93cb69a
Jonathan White <jc.white18@outlook.com>
2020-02-22 11:31:34 -0600

    Added burger transition to css
    Still need to fix menu on mobile

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ const navSlide = () => {
 
     burger.addEventListener('click',()=>{
         // Toggle Nav
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle('.nav-active');
 
         //Animate Links
         navLinks.forEach((link, index) => { 
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
             }
         });
         // Burger Animation
---
+++ js/navtrans.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('.burger').addEventListener('click',
+function(){
+    document.querySelector('.nav-links').classList.toggle('.nav-active');
+
+    document.querySelectorAll('.nav-links li')
+})
